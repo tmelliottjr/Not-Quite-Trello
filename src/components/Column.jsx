f83074b642c54fe6eeff4a321d7206dadef21f9d
Thomas Elliott <tom@mbp.localdomain>
2019-02-21 00:03:10 -0500

    update column sizing

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -11,7 +11,7 @@ import { DeleteColumnButton } from './DeleteColumnButton';
 
 const Container = styled.div`
   flex-direction: column;
-  flex: 0 0 255px;
+  flex: 0 0 256px;
   margin: 10px;
   border-radius: 2px;
   background-color: #353d42ad;
